a876bea566deef7bac1d9c5fc790d1936a958fab
liwei <328090991@qq.com>
2019-03-12 14:37:45 +0800

    filter

---
+++ spring-cloud-learn/cloud-zuul/src/main/java/com/cloud/zuul/MyFilter.java
@@ -0,0 +1,41 @@
+package com.cloud.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by wei.li
+ * on 2019/3/12
+ */
+@Component
+public class MyFilter extends ZuulFilter{
+
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletResponse response = requestContext.getResponse();
+        response.setHeader("haader", "test");
+        return null;
+    }
+}
